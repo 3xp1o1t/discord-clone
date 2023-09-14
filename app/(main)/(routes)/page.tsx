eaf0b5a2badcf77274ab8e7d61cbc090bd8b9bd9
3xp1o1t <jes.monti.503@gmail.com>
2023-09-14 15:51:03 -0600

    Added: Folder setup structure
    Added a folder setup structure.

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from '@/components/ui/button';
+
 export default function Home() {
   return (
     <div className="test">
       <h1 className="text-3xl font-bold text-indigo-500">hi!</h1>
+      <Button>Click me!</Button>
     </div>
   );
 }
