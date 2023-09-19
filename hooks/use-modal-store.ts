00616ab55fdad66a6966ed03dba295f1c14bdb4b
3xp1o1t <jes.monti.503@gmail.com>
2023-09-18 23:56:07 -0600

    Added: Manage members funtionallity
    Added functionality to manage members button
    Only admin can manage members like kick from server or switch roles.
    Added modal for list members
    Added some routes for manage members

--- hooks/use-modal-store.ts
+++ hooks/use-modal-store.ts
@@ -2,7 +2,7 @@ import { Server } from '@prisma/client';
 import { create } from 'zustand';
 
 // El tipo de accion que hara el modal.
-export type ModalType = 'createServer' | 'invite' | 'editServer';
+export type ModalType = 'createServer' | 'invite' | 'editServer' | 'members';
 
 interface ModalData {
   server?: Server;
